616728d1fb6d1c4c7bbccca1171e3113dc24d4b3
Joshua Farayola <farayolajoshua@gmail.com>
2023-09-21 09:01:01 +0100

    feat: implement login route

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,5 +1,43 @@
+import { ZodError, z } from "zod";
+import { fromZodError } from "zod-validation-error";
+import { validateUser } from "../models/users.js";
+
 export async function getLoginPage(req, res) {
   res.render("login");
+}
+
+export async function login(req, res) {
+  const schema = z
+    .object({
+      email: z.string().email(),
+      password: z.string(),
+    })
+    .required();
+
+  try {
+    const { email, password } = schema.parse(req.body);
+
+    const user = await validateUser({ email, password });
+    req.session.user = user;
+
+    return res.redirect("/");
+  } catch (error) {
+    let errMessage;
+
+    if (error instanceof ZodError) {
+      errMessage = fromZodError(error);
+    } else {
+      errMessage = error.message;
+    }
+
+    return res.render("login", {
+      error: errMessage,
+      data: {
+        email: req.body.email,
+        password: req.body.password,
+      },
+    });
+  }
 }
 
 export async function getSignupPage(req, res) {
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { getLoginPage, getSignupPage } from "../controllers/auth.js";
+import { getLoginPage, getSignupPage, login } from "../controllers/auth.js";
 
 const authRouter = Router();
 
 authRouter.get("/login", getLoginPage);
+authRouter.post("/login", login);
 authRouter.get("/signup", getSignupPage);
 
 export default authRouter;
